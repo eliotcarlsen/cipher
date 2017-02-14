8547afb4d61d4c18ec2fc2e014ecfdaf5b5c3943
eliot.carlsen <eliot.carlsen@gmail.com>
2017-02-14 08:03:32 -0800

    started cipher project

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var sentence = prompt("Give me a sentence!");
+
+var capitalizeLetters = function(string) {
+ var firstLastLetter = str.charAt(str.length-1);
+  var firstLastCap = firstLastLetter.upperCase();
+  return capLastLetter;
+};
+
+alert(capitalizeLetters(sentence))
